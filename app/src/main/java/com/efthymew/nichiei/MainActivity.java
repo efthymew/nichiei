37e98674513d67dc81368adcc07d11521d0f9bc1
graham <grahomie@gmail.com>
2020-09-27 02:38:52 -0400

    testing something again

--- app/src/main/java/com/efthymew/nichiei/MainActivity.java
+++ app/src/main/java/com/efthymew/nichiei/MainActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 // commenting just so i can push
+// test again
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
